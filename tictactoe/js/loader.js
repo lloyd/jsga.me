e284cdee55f8ace0d16de88ffef9c4a0cc7d54f7
Lloyd Hilaiel <lloyd@hilaiel.com>
2011-04-30 08:25:24 -0600

    whitespace changes

--- tictactoe/js/loader.js
+++ tictactoe/js/loader.js
@@ -1,10 +1,10 @@
 var chan = Channel.build({
-  window: window.parent,
-  origin: "*",
-  scope: "jsga.me"
+    window: window.parent,
+    origin: "*",
+    scope: "jsga.me"
 });
 
 chan.bind("load", function(trans, s) {
-  console.log("I'm supposed to load " + s);
-  return "loaded";
+    console.log("loader.js: I'm supposed to load " + s);
+    return "loaded";
 });
--- tictactoe/js/main.js
+++ tictactoe/js/main.js
@@ -1,44 +1,44 @@
 $(document).ready(function() {
-  var contestants = {
-  };
-
-  $.get("contestants/all.txt", function (data) {
-    $("#contestants").text(data);
-    var contestants = data.split("\n");
-    for (var i = 0; i < contestants.length; i++) {
-      var c = $.trim(contestants[i]);
-      // ignore blank contestants
-      if (c.length == 0) continue;
-
-      console.log("loading contestant: ", c);
-
-      // allocate an iframe for each contestant
-      var iframe = $("<iframe/>")
-        .addClass("bot")
-        .attr("src", "loader.html");
-
-      iframe.appendTo($("body"));
-
-      // wrap the iframe in a jschannel for communication
-      var chan = Channel.build({
-        window: iframe.get(0).contentWindow,
-        origin: "*",
-        scope: "jsga.me"
-      });
-
-      // pass in the script name for the loader to load
-      chan.call({
-        method: "load",
-        params: c,
-        success: function(v) {
-          console.log("sign of life from child!");
-        }
-      });
-
-      contestants[c] = {
-        chan: chan,
-        iframe: iframe
-      };
+    var contestants = {
     };
-  }, "text" );
+
+    $.get("contestants/all.txt", function (data) {
+        $("#contestants").text(data);
+        var contestants = data.split("\n");
+        for (var i = 0; i < contestants.length; i++) {
+            var c = $.trim(contestants[i]);
+            // ignore blank contestants
+            if (c.length == 0) continue;
+
+            console.log("loading contestant: ", c);
+
+            // allocate an iframe for each contestant
+            var iframe = $("<iframe/>")
+                .addClass("bot")
+                .attr("src", "loader.html");
+
+            iframe.appendTo($("body"));
+
+            // wrap the iframe in a jschannel for communication
+            var chan = Channel.build({
+                window: iframe.get(0).contentWindow,
+                origin: "*",
+                scope: "jsga.me"
+            });
+
+            // pass in the script name for the loader to load
+            chan.call({
+                method: "load",
+                params: c,
+                success: function(v) {
+                    console.log("sign of life from child!");
+                }
+            });
+
+            contestants[c] = {
+                chan: chan,
+                iframe: iframe
+            };
+        };
+    }, "text" );
 });
