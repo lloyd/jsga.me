38c94260e599e366e7c38c6a54a9172544ef0dca
Lloyd Hilaiel <lloyd@hilaiel.com>
2011-04-30 09:13:08 -0600

    small checkpoint, board representation and winner determination

--- tictactoe/js/main.js
+++ tictactoe/js/main.js
@@ -36,9 +36,61 @@ $(document).ready(function() {
             });
 
             contestants[c] = {
+                name: c,
                 chan: chan,
                 iframe: iframe
             };
         };
     }, "text" );
+
+    // checks board state and returns 'x', 'o', or null
+    function winner() {
+        var board = [];
+        $("#board > div.square").each(function(e) {
+            var v = e.text();
+            if (v != "X" && v != 'O') v = null;
+            board.push(v);
+        });
+
+        // check horizontals
+        if (board[0] && (board[0] == board[1] == board[2])) return board[0];
+        if (board[3] && (board[3] == board[4] == board[5])) return board[3];
+        if (board[6] && (board[6] == board[7] == board[8])) return board[6];
+
+        // check verticals
+        if (board[0] && (board[0] == board[3] == board[6])) return board[0];
+        if (board[1] && (board[1] == board[4] == board[7])) return board[1];
+        if (board[2] && (board[2] == board[5] == board[8])) return board[2];
+
+        // check diagonals
+        if (board[0] && (board[0] == board[4] == board[8])) return board[0];
+        if (board[2] && (board[2] == board[4] == board[6])) return board[2];
+
+        return null;
+    }
+    
+    $("#startButton").click(function() {
+        // let's just run a tourney between the first two contestants as a proof of
+        // concept
+
+        // clear board
+        $("#board > div.square").text("");
+
+        var players = {
+            X: contestants[0],
+            O: contestants[1]
+        };
+
+        function nextTurn() {
+            var w = winner();
+            if (w) {
+                alert("Game over!  " + players[w].name + " wins!");
+            } else {
+
+            }
+        }
+
+        runTurn();
+    });
+
 });
