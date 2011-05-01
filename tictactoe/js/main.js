f9c5a40f5b7e6242916f0db05d9243bd0154f675
Lloyd Hilaiel <lloyd@hilaiel.com>
2011-05-01 07:43:10 -0600

    checkpoint: let user control who plays, run only one iteration of a game with the top two players

--- tictactoe/js/main.js
+++ tictactoe/js/main.js
@@ -1,46 +1,62 @@
 $(document).ready(function() {
-    var contestants = [];
+    // contestants have ids to be cross referenced in various places.
+    var curid = 0;
 
+    var contestants = {
+    };
+
+    function addPlayer(file) {
+        // allocate an iframe for each contestant
+        var iframe = $("<iframe/>")
+            .addClass("contestant")
+            .attr("src", "loader.html");
+
+        iframe.appendTo($("body"));
+
+        // wrap the iframe in a jschannel for communication
+        var chan = Channel.build({
+            window: iframe.get(0).contentWindow,
+            origin: "*",
+            scope: "jsga.me"
+        });
+
+        // pass in the script name for the loader to load
+        chan.call({
+            method: "load",
+            params: file,
+            success: function(v) {
+                console.log("sign of life from child!");
+                contestants["player_" + (curid++).toString()] = {
+                    name: file,
+                    chan: chan,
+                    iframe: iframe
+                };
+                updateContestantsInDOM();
+            }
+        });
+    }
+    
     $.get("contestants/all.txt", function (data) {
+
         var contestantFiles = data.split("\n");
         for (var i = 0; i < contestantFiles.length; i++) {
-            alert(c);
             var c = $.trim(contestantFiles[i]);
             // ignore blank contestants
             if (c.length == 0) continue;
 
-            console.log("loading contestant: ", c);
-
-            // allocate an iframe for each contestant
-            var iframe = $("<iframe/>")
-                .addClass("contestant")
-                .attr("src", "loader.html");
-
-            iframe.appendTo($("body"));
-
-            // wrap the iframe in a jschannel for communication
-            var chan = Channel.build({
-                window: iframe.get(0).contentWindow,
-                origin: "*",
-                scope: "jsga.me"
-            });
-
-            // pass in the script name for the loader to load
-            chan.call({
-                method: "load",
-                params: c,
-                success: function(v) {
-                    console.log("sign of life from child!");
-                }
-            });
-
-            contestants.push({
-                name: c,
-                chan: chan,
-                iframe: iframe
-            });
+            addPlayer(c);
         };
     }, "text" );
+
+    function updateContestantsInDOM() {
+        $("#contestants").empty();
+        for (var id in contestants) {
+            var cn = $("#templates > div.contestant").clone();
+            cn.find(".name").text(contestants[id].name);
+            cn.find("input").attr("player_id", id);
+            cn.appendTo($("#contestants"));
+        }
+    }
 
     // checks board state and returns 'x', 'o', 'tie', or null
     function winner() {
@@ -72,26 +88,16 @@ $(document).ready(function() {
 
         return null;
     }
-    
-    $("#startButton").click(function() {
-        // let's just run a tourney between the first two contestants as a proof of
-        // concept
 
-        // clear board
-        $("#board > div.square").text("");
-
-        var players = {
-            "X": contestants[2],
-            "O": contestants[0]
-        };
-
+    // run a single game with the players as specified in the players map, who has
+    // two interesting keys, 'X' and 'O'
+    function runOneGame(players, cb) {
         var curPlayer = "X";
 
         function nextTurn() {
             var w = winner();
             if (w) {
-                if (w === 'tie') alert("Game over!  It's a tie!");
-                else alert("Game over!  " + players[w].name + " wins!");
+                cb(w); // either X, O, or tie
             } else {
                 var board = [];
                 var map = {
@@ -107,15 +113,41 @@ $(document).ready(function() {
                     params: board,
                     success: function(v) {
                         $("#board > div.square:nth-child("+(v+1)+")").text(curPlayer);
-                        setTimeout(nextTurn, 1000);
+                        curPlayer = (curPlayer === "X") ? "O" : "X";
+                        setTimeout(nextTurn, 700);
                     }
                 });
-
-                curPlayer = (curPlayer === "X") ? "O" : "X";
             }
         }
 
         nextTurn();
+    }
+
+    function getSelectedPlayers() {
+        var players = [];
+        $("#contestants input:checked").each(function(i, e) {
+            players.push(contestants[$(e).attr('player_id')]);
+        });
+        return players;
+    }
+    
+    $("#startButton").click(function() {
+        // let's just run a tourney between the first two contestants as a proof of
+        // concept
+
+        // clear board
+        $("#board > div.square").text("");
+
+        var players = getSelectedPlayers();
+
+        // XXX: implement round robin tourney logic.
+        runOneGame({
+            X: players[0],
+            O: players[1]
+        }, function (status) {
+            if (status === 'tie') alert("Game over!  It's a tie!");
+            else alert("Game over!  " + status + " wins!");
+        });
     });
 
 });
